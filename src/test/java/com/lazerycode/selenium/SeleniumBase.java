84f3f28c110b3af0aa32efac0f7b96c6322356a2
Matt <matt.black7@gmail.com>
2014-08-05 20:29:34 -0700

    add version specification, switch around some stuff to work better on Spoonium

--- src/test/java/com/lazerycode/selenium/SeleniumBase.java
+++ src/test/java/com/lazerycode/selenium/SeleniumBase.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -23,6 +24,7 @@ public class SeleniumBase {
 
     private static ResourceBundle _prop = ResourceBundle.getBundle("dev");
     private static BrowserType browserType;
+    private static String browserVersion;
     private static List<WebDriver> webDrivers = Collections.synchronizedList(new ArrayList<WebDriver>());
     private static ThreadLocal<WebDriver> driverForThread = new ThreadLocal<WebDriver>() {
 
@@ -71,7 +73,8 @@ public class SeleniumBase {
                 capabilities = DesiredCapabilities.internetExplorer();
                 capabilities.setCapability(CapabilityType.ForSeleniumServer.ENSURING_CLEAN_SESSION, true);
                 capabilities.setCapability(InternetExplorerDriver.ENABLE_PERSISTENT_HOVERING, true);
-                capabilities.setCapability("requireWindowFocus", true);
+                capabilities.setCapability(InternetExplorerDriver.FORCE_CREATE_PROCESS, true);
+                capabilities.setCapability(InternetExplorerDriver.IE_SWITCHES, "-private"); //always launch in private mode
                 break;
             case SAFARI:
                 capabilities = DesiredCapabilities.safari();
@@ -117,6 +120,13 @@ public class SeleniumBase {
                 capabilities = DesiredCapabilities.htmlUnit();
                 capabilities.setCapability("javascriptEnabled", "true");
         }
+        //set the version, if specified
+        browserVersion = _prop.getString("version");
+        if (browserVersion.equals("0")) {
+            //do nothing
+        } else {
+            capabilities.setCapability(CapabilityType.VERSION, browserVersion);
+        }
 
         return capabilities;
     }
@@ -124,16 +134,16 @@ public class SeleniumBase {
     private static WebDriver loadWebDriver() {
         System.out.println("Current Operating System: " + System.getProperties().getProperty("os.name"));
         System.out.println("Current Architecture: " + System.getProperties().getProperty("os.arch"));
-        System.out.println("Current Browser Selection: " + browserType);
+        System.out.println("Current Browser Selection: " + browserType + " " + browserVersion);
 
         //Instantiate driver object
         switch (browserType) {
             case FIREFOX:
-                return new FirefoxDriver(generateDesiredCapabilities(browserType));
+                return new RemoteWebDriver(generateDesiredCapabilities(browserType));
             case CHROME:
-                return new ChromeDriver(generateDesiredCapabilities(browserType));
+                return new RemoteWebDriver(generateDesiredCapabilities(browserType));
             case IE:
-                return new InternetExplorerDriver(generateDesiredCapabilities(browserType));
+                return new RemoteWebDriver(generateDesiredCapabilities(browserType));
             case SAFARI:
                 return new SafariDriver(generateDesiredCapabilities(browserType));
             case OPERA:
